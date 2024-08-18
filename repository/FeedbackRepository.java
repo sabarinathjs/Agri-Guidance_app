39b78ef0de4162bc0c15b2bf2e706a63fad175b4
sabarinathjs <150121729+sabarinathjs@users.noreply.github.com>
2024-08-18 17:50:17 +0530

    Delete repository directory

--- repository/FeedbackRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.model.Feedback;
-
-public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-}
